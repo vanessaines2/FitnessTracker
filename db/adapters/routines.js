d5b8562b6f6c9c623cbac519b591347196b9fab2
Vanessaines2 <Vanessaines2@icloud.com>
2023-05-22 12:26:13 -0400

    created functions

--- db/adapters/routines.js
+++ db/adapters/routines.js
@@ -1,1 +1,14 @@
 const client = require("../client");
+
+async function getRoutineById() {}
+
+async function getRoutinesWithoutActivities() {}
+
+async function getAllRoutines() {}
+async function getAllPublicRoutines() {}
+async function getAllRoutinesByUser() {}
+async function getPublicRoutinesByUser() {}
+async function getPublicRoutinesByActivity() {}
+async function createRoutine() {}
+async function updateRoutine() {}
+async function destroyRoutine() {}
--- db/adapters/users.js
+++ db/adapters/users.js
@@ -1,1 +1,9 @@
 const client = require("../client");
+
+async function createUser() {}
+
+async function getUser() {}
+
+async function getUserById() {}
+
+async function getUserByUsername() {}
