0795795204cb69799ae567e8375a026a26f7af84
Wanda Jones <wandam.jones0704@yahoo.com>
2023-05-25 03:44:49 -0400

    Routine activities adapters done

--- db/adapters/routine_activities.js
+++ db/adapters/routine_activities.js
@@ -1,13 +1,90 @@
 const client = require("../client");
 
-async function getRoutineActivityById() {}
+async function getRoutineActivitiesByRoutine(routineId) {
+  try {
+    const {
+      rows: [routineActivity],
+    } = await client.query(
+      ` SELECT *
+            FROM routine-activities
+            WHERE routine_id = $1`,
+      [routineId]
+    );
+    return routineActivity;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function addActivityToRoutine() {}
+async function getRoutineActivityById(routineActivityId) {
+  try {
+    const {
+      rows: [routineActivity],
+    } = await client.query(
+      `SELECT * 
+            FROM routine_activities
+            WHERE id=$1`,
+      [routineActivityId]
+    );
+    return routineActivity;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function updateRoutineActivity() {}
+async function addActivityToRoutine(routineId, activityId, count, duration) {
+  try {
+    const { rows } = await client.query(
+      `
+            INSERT INTO routine_activities(routine_id, activity_id, count, duration)
+            VALUES($1, $2, $3, $4)
+            RETURN *
+            `,
+      [routineId, activityId, count, duration]
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function destroyRoutineActivity() {}
+async function updateRoutineActivity(routineActivityId, count, duration) {
+  try {
+    const {
+      rows: [routineActivity],
+    } = await client.query(
+      ` UPDATE routine_activities
+            SET "count" = $2, "duration" =$3
+            WHERE id= $1
+            RETURN * `,
+      [routineActivityId, count, duration]
+    );
+    return routineActivity;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function getRoutineActivitiesByRoutine() {}
+async function destroyRoutineActivity(routineActivityId) {
+  try {
+    const res = await client.query(
+      `
+      DELETE FROM routine_activities
+      WHERE id=$1;
+    `,
+      [routineActivityId]
+    );
 
-module.exports = {};
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getRoutineActivitiesByRoutine,
+  getRoutineActivityById,
+  addActivityToRoutine,
+  updateRoutineActivity,
+  destroyRoutineActivity,
+};
