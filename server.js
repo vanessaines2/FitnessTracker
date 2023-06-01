ca49e5ea969e961802d658d7df16eea005b373ba
Vanessaines2 <Vanessaines2@icloud.com>
2023-06-01 00:36:43 -0400

    have added react to our express app, you may have to install packages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const PORT = 3001;
@@ -11,8 +12,14 @@ client.connect();
 server.use(morgan("dev"));
 server.use(express.json());
 
+server.use(express.static(path.join(__dirname, "./client", "dist")));
+
 // Routes
 server.use("/api", require("./routes"));
+
+server.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
+});
 
 // Error Handler
 server.use((err, req, res, next) => {
