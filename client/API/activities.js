30afd5c77f200fac560a18b7002b34307785d767
Vanessaines2 <Vanessaines2@icloud.com>
2023-06-01 11:47:44 -0400

    created activities component and got to display activities on page

--- client/API/activities.js
+++ client/API/activities.js
@@ -2,8 +2,8 @@ export async function fetchAllActivities() {
   try {
     const response = await fetch("/api/activities");
     const result = await response.json();
-    console.log("result: ", result);
-    return result;
+    console.log("result data: ", result.data, "Just result : ", result);
+    return result.data;
   } catch (error) {
     console.log(error);
   }
--- client/Components/AllActivities.jsx
+++ client/Components/AllActivities.jsx
@@ -3,7 +3,7 @@ import { fetchAllActivities } from "../API/activities";
 
 export function AllActivities() {
   const [activities, setActivities] = useState("");
-  const [error, setError] = useState(null);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
     async function getActivities() {
@@ -13,5 +13,20 @@ export function AllActivities() {
     getActivities();
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="activitiesPage">
+      <h1>All Activities</h1>
+      {activities.length > 0 &&
+        activities.map((activity) => {
+          return (
+            <div className="activities" key={activity.id}>
+              <ul className="activitiesList" style={{ listStyle: "none" }}>
+                <li> Activity Name: {activity.name}</li>
+                <li>Activity Description: {activity.description}</li>
+              </ul>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
