090c484e8423575301293ef30603a3d2057adf06
Vanessaines2 <Vanessaines2@icloud.com>
2023-06-07 13:48:31 -0400

    register user

---
+++ client/API/registerUser.js
@@ -0,0 +1,21 @@
+export async function registerUser(username, password) {
+  try {
+    const response = await fetch("/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
