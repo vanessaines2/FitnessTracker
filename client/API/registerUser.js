37d6930f1fd251dc865862a43203d56bfbbedc0d
Vanessaines2 <Vanessaines2@icloud.com>
2023-06-08 11:27:37 -0400

    users api helper functions, front end

--- client/API/registerUser.js
+++ client/API/registerUser.js
@@ -17,3 +17,54 @@ export async function registerUser(username, password) {
     console.log(error);
   }
 }
+export async function loginUser(username, password) {
+  try {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchMe() {
+  try {
+    const response = await fetch("/api/auth/me");
+    const { success, message, user } = await response.json();
+    if (!success) {
+      throw {
+        message,
+      };
+    }
+    return {
+      success,
+      message,
+      user,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function logOut() {
+  const response = await fetch("/api/auth/logout");
+  const { success, message } = await response.json();
+  if (!success) {
+    throw {
+      message,
+    };
+  }
+  return {
+    success,
+    message,
+  };
+}
---
+++ client/Components/Profile.jsx
@@ -0,0 +1,1 @@
+import { fetchMe } from "../API/registerUser";
