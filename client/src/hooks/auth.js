df20e57045cde07ed70b17b33de4ba1da1fe13a5
Wanda Jones <wandam.jones0704@yahoo.com>
2023-06-11 11:32:19 -0400

    syntax

--- client/src/hooks/auth.js
+++ client/src/hooks/auth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "../components/auth/AuthProvider";
+import { AuthContext } from "../Components/auth/AuthProvider";
 
 const useAuth = () => {
   return useContext(AuthContext);
