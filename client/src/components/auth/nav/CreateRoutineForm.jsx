141a51ea0eb125280638bc3ab111010526b5006f
Vanessaines2 <Vanessaines2@icloud.com>
2023-06-11 15:35:39 -0400

    create routine attempt

--- client/src/components/auth/nav/CreateRoutineForm.jsx
+++ client/src/components/auth/nav/CreateRoutineForm.jsx
@@ -4,29 +4,35 @@ import { createRoutine } from "../../../api/routines";
 export default function CreateRoutine() {
   const [name, setName] = useState("");
   const [goal, setGoal] = useState("");
-  const [isPublic, setIsPublic] = useState("");
+  const [is_public, setIs_Public] = useState(false);
   const [myCreatedRoutines, setMyCreatedRoutines] = useState("");
 
-  //   useEffect(() => {
-  //     async function createMyRoutine(name, goal, creatorId, isPublic) {
-  //       const response = await createRoutine(name, goal, creatorId, isPublic);
-  //       setMyCreatedRoutines(response);
-  //     }
-  //     createMyRoutine();
-  //   }, []);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const result = createRoutine(is_public, name, goal);
+      console.log(result);
+      setMyCreatedRoutines(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="routineForm">
       <h1>Create Routine</h1>
-      <form
-        className="routineForm"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await createRoutine(isPublic, name, goal);
-          console.log("this is the created routine", createRoutine);
-        }}
-      >
-        <label>enter routine name</label>
+      <form className="routineForm" onSubmit={handleSubmit}>
+        <label>enter routine name</label>{" "}
+        <input
+          type="checkbox"
+          value={is_public}
+          placeholder="is it public"
+          onChange={(e) => {
+            e.preventDefault;
+
+            setIs_Public(e.target.value);
+          }}
+        />
         <input
           type="text"
           value={name}
@@ -45,16 +51,6 @@ export default function CreateRoutine() {
             e.preventDefault;
 
             setGoal(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          value={isPublic}
-          placeholder="is it public"
-          onChange={(e) => {
-            e.preventDefault;
-
-            setIsPublic(e.target.value);
           }}
         />
         <button>Submit </button>
